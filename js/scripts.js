5acb56e4100f37ec1b67461962772fbfe72f0e23
Pete Lazuran and Roger Tobias <pete@>
2017-05-03 10:39:49 -0700

    HTML, CSS ready, if function for finding vowels complete and working.

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+ $("form#mainform").submit(function(event){
+   var sentence = $("input#sentence").val();
+   var seperate = sentence.split(" ");
+   
+
+
+
+   event.preventDefault()
+ });
+});
+
+  if (array[0] === "a" || array[0] === "e" || array[0] === "i" || array[0] === "o" || array[0] === "u"|| array[0] === "A" || array[0] === "E" || array[0] === "I" || array[0] === "O" || array[0] === "U"){
+    alert(array + "ay");
+  };
+
+for (var i = 0 ; array)
